11bf9a18554a2448bd098781bf043e3906be9c8e
Aastha <saastha930@gmail.com>
2025-02-12 01:20:19 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,10 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-
 // Middleware
-
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true,
   }));
 
